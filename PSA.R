001a2ac6b71399dece023a03ea7ed989f6c085d9
Jonah <jonah_popp@brown.edu>
2023-07-27 17:54:01 -0400

    fix date bug in excel and updated PSA code

--- PSA.R
+++ PSA.R
@@ -1,34 +1,87 @@
 user = 'Jonah'
 
-
-#load packages
+##load packages
 library(tidyverse)
 
 
-#Set working directory
-jonah_work_dir <- "C:/Users/Jonah/Dropbox (Brown)/Shared Stuff/DHAKA & NIRUDAK Cost-effectiveness analysis"
+##Set working directory
+jonah_work_dir <- "C:/Users/Jonah/Desktop/NIRUDAK_CEA"
 
 if (user=='Jonah'){
 	setwd(jonah_work_dir)	
 }
 
-#load data
-tmp_data <- readxl::read_excel(file.path("previous work", "NIRUDAK_over5yrs_raw_data_.xlsx"))
-names(tmp_data)
-data <- tmp_data %>% rename(id="Study ID") %>% rename(admit_date="Admit Date") %>% 
+##load data and clean
+tmp_data <- readxl::read_excel(file.path("data", "NIRUDAK_over5yrs_raw_data_.xlsx"))
+data_tmp1 <- tmp_data %>% rename(id="Study ID") %>% rename(admit_date="Admit Date") %>% 
 		rename(admit_time="Admit Time") %>% mutate(male=as.integer(Sex=="Male")) %>%
 		select(-c(Sex, sex1)) %>% rename(age=Age) %>% rename(admit_wt="Admit Weight") %>%
-		rename(
+		rename(iv_fluid_prior_to_admit_wt="IV Fluid Prior to Getting Admit Weight")
+fu_names_to_change <- names(data_tmp1)[7:76]
+colnames(data_tmp1)[7:76] <- gsub("Fluid", "fluid", gsub("Time", "time", gsub("Date", "data", 
+		gsub("Patient_Weight", "wt", gsub(" ", "_", gsub("FU ", "fu", gsub("Form [6-8]::", "", fu_names_to_change)))))))
+colnames(data_tmp1) <- gsub("Discharge Weight", "discharge_wt", gsub("Discharge Time", "discharge_time", 
+					gsub("Discharge Date", "discharge_date", gsub("Form [8-9]::", "", colnames(data_tmp1)))))
+colnames(data_tmp1) <- gsub("Date", "date", gsub("Weight", "wt", gsub("Time", "time", gsub("Return ", "return_", colnames(data_tmp1)))))
+data_tmp2 <- data_tmp1 %>% rename(final_wt="Final wt") %>% rename(stable_wt="Stable wt") %>% 
+			rename(actual_percent_dehydrat="Actual Percent Dehdyration") %>%
+			rename(actual_dehydrat_cat="Actual Dehydration Category") %>%
+ 			rename(nirudak_percent_dehydrat="Model 6 Percent Dehydration") %>% 
+			rename(nirudak_dehydrat_cat="Model 6 Dehydration Category") %>% 
+			rename(nirudak_volume_deficit="Model 6 Volume Deficit (L)") %>% 
+			rename(who_dehydrat_cat="WHO Dehydration Category") %>% 
+			rename(who_volume_deficit="WHO Volume Deficit (L)") %>% 
+			rename(who_severe_dehydrat="WHO severe dehydration") %>% 
+			rename(who_some_dehydrat="WHO some dehydration")
 
-with(tmp_data, Age)
+
+##Construct Needed Variables
+
+data <- data_tmp2 %>% mutate(admit_date_time=as.POSIXct(paste(as.Date(admit_date), 
+							format(admit_time, "%H:%M:%S")), tz="UTC", format="%Y-%m-%d %H:%M:%S")) %>%
+		mutate(discharge_date_time=as.POSIXct(paste(as.Date(discharge_date), 
+							format(discharge_time, "%H:%M:%S")), tz="UTC", format="%Y-%m-%d %H:%M:%S")) %>%
+		mutate(hosp_los = difftime(discharge_date_time, admit_date_time, units='hours', 2))
+data <- data %>% mutate(hosp_los = round(hosp_los, 2))
+
+data %>% select(hosp_los) %>% subset(hosp_los < 0)
 
 
-tmp_data %>% names()
+
+round(with(data, difftime(discharge_date_time, admit_date_time, units='hours', 2)), 2)
+?difftime
+data$hosp_los
+
+gen hospital_cost_usd = length_stay_hrs * 30
+gen hospital_cost_bdt = length_stay_hrs * 2573.1
+
+
+    
+
+####Unit Costs
+
+##IV and ORS Costs in BDT
+IV_tube_and_solution_price <- 14.58
+pair_gloves_price <- 6.86
+butterfly_needle_price <- 11.15
+IV_fixed_unit_cost <- IV_tube_and_solution_price + pair_gloves_price + butterfly_needle_price
+IV_fluid_cost_per_ml <- 0.104
+ORS_fluid_cost_per_ml <- 0.0054
+
+
+##Hospital Costs in BDT
+hosp_cost_per_hour <- 2573.1/24
 
 
 
 
-?drop
+
+
+
+
+
+
+
 
 
 
