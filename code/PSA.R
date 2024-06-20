c22394e5cf69134993d6af4a8f296104488da07a
Anagha Lokhande <alokhande21@gmail.com>
2024-06-20 11:27:27 -0400

    added in Stata code

--- code/PSA.R
+++ code/PSA.R
@@ -1,4 +1,5 @@
-user = 'Jonah'
+# user = 'Jonah'
+user = 'Anagha'
 
 ##load packages
 library(tidyverse)
@@ -7,8 +8,14 @@ library(tidyverse)
 ##Set working directory
 jonah_work_dir <- "C:/Users/Jonah/Desktop/NIRUDAK_CEA"
 
+anagha_work_dir <- "/Users/anaghalokhande/Desktop/Research/Levine-NIRUDAK-CEA"
+
 if (user=='Jonah'){
 	setwd(jonah_work_dir)	
+}
+
+if (user=='Anagha'){
+  setwd(anagha_work_dir)	
 }
 
 
@@ -350,7 +357,175 @@ treating those with some as if they had no dehydration
   #assume treating those with no dehydration as if they had some dehydration carries no DALY penalty, only a cost 
 
 
-
+# define net heatlh benefit function
+ICER_fxn <- function(death_gvn_overtreat_prob, death_gvn_undertreat_prob) {
+  
+  
+  # defining all expected costs and all expected DALYs
+  # WHO prob
+  branch_a <- 0.06812325
+  branch_b <- 0.23498925
+  branch_c <- 0.0393875
+  branch_d <- 0.05125032
+  branch_e <- 0.30279966
+  branch_f <- 0.07735002
+  branch_g <- 0.00988057
+  branch_h <- 0.13161281
+  branch_i <- 0.08460662
+  
+  # NIRUDAK
+  branch_j <- 0.10285635
+  branch_k <- 0.28938165
+  branch_l <- 0.032262
+  branch_m <- 0.02196501
+  branch_n <- 0.21924172
+  branch_o <- 0.05049327
+  branch_p <- 0.0046827
+  branch_q <- 0.16037538
+  branch_r <- 0.1187703
+  
+  # WHO costs
+  branch_a_cost <- 97.04
+  branch_b_cost <-100.36
+  branch_c_cost <-102.37
+  branch_d_cost <-27.91
+  branch_e_cost <-26.65
+  branch_f_cost <-17.19
+  branch_g_cost <-9.92
+  branch_h_cost <-12.87
+  branch_i_cost <-14.37
+  
+  # NIRUDAK costs
+  branch_j_cost <-96.82
+  branch_k_cost <-93.91
+  branch_l_cost <-96.02
+  branch_m_cost <-26.09
+  branch_n_cost <-27.27
+  branch_o_cost <-28.92
+  branch_p_cost <-16.53
+  branch_q_cost <-14.17
+  branch_r_cost <-6.88
+  
+  # listing mean DALYs lost conditional on dying; doesn't vary w/ sensitivity analysis
+  # WHO
+  # death from overtreatment
+  mean_DALYs_lost_if_die_branch_b <- 44.97
+  mean_DALYs_lost_if_die_branch_c <- 35.09
+  # death from undertreatment
+  mean_DALYs_lost_if_die_branch_d <- 58.99
+  mean_DALYs_lost_if_die_branch_g <- 64.02
+  
+  # NIRUDAK
+  # death from overtreatment
+  mean_DALYs_lost_if_die_branch_k <- 52.41
+  mean_DALYs_lost_if_die_branch_l <- 49.18
+  # death from undertreatment
+  mean_DALYs_lost_if_die_branch_m <- 48.32
+  mean_DALYs_lost_if_die_branch_p <- 49.92
+  
+  # expected DALYs lost conditional on making it to that branch endpoint
+  # WHO
+  branch_a_DALY <- 0
+  branch_b_DALY <- death_gvn_overtreat_prob*mean_DALYs_lost_if_die_branch_b
+  branch_c_DALY <- death_gvn_overtreat_prob*mean_DALYs_lost_if_die_branch_c
+  branch_d_DALY <- death_gvn_undertreat_prob*mean_DALYs_lost_if_die_branch_d
+  branch_e_DALY <- 0
+  branch_f_DALY <- 0
+  branch_g_DALY <- death_gvn_undertreat_prob*mean_DALYs_lost_if_die_branch_g
+  branch_h_DALY <- 0
+  branch_i_DALY <- 0
+  
+  # NIRUDAK
+  branch_j_DALY <- 0
+  branch_k_DALY <- death_gvn_overtreat_prob*mean_DALYs_lost_if_die_branch_k
+  branch_l_DALY <- death_gvn_overtreat_prob*mean_DALYs_lost_if_die_branch_l
+  branch_m_DALY <- death_gvn_undertreat_prob*mean_DALYs_lost_if_die_branch_m
+  branch_n_DALY <- 0
+  branch_o_DALY <- 0
+  branch_p_DALY <- death_gvn_undertreat_prob*mean_DALYs_lost_if_die_branch_p
+  branch_q_DALY <- 0
+  branch_r_DALY <- 0
+  
+  # calculating expected costs
+  # WHO
+  branch_a_expected_cost <- branch_a_cost*branch_a
+  branch_b_expected_cost <- branch_b_cost*branch_b
+  branch_c_expected_cost <- branch_c_cost*branch_c
+  branch_d_expected_cost <- branch_d_cost*branch_d
+  branch_e_expected_cost <- branch_e_cost*branch_e
+  branch_f_expected_cost <- branch_f_cost*branch_f
+  branch_g_expected_cost <- branch_g_cost*branch_g
+  branch_h_expected_cost <- branch_h_cost*branch_h
+  branch_i_expected_cost <- branch_i_cost*branch_i
+  
+  #NIRUDAK
+  branch_j_expected_cost <- branch_j_cost*branch_j
+  branch_k_expected_cost <- branch_k_cost*branch_k
+  branch_l_expected_cost <- branch_l_cost*branch_l
+  branch_m_expected_cost <- branch_m_cost*branch_m
+  branch_n_expected_cost <- branch_n_cost*branch_n
+  branch_o_expected_cost <- branch_o_cost*branch_o
+  branch_p_expected_cost <- branch_p_cost*branch_p
+  branch_q_expected_cost <- branch_q_cost*branch_q
+  branch_r_expected_cost <- branch_r_cost*branch_r
+  
+  # calculating expected DALYs
+  # WHO
+  branch_a_expected_DALY <- branch_a_DALY*branch_a
+  branch_b_expected_DALY <- branch_b_DALY*branch_b
+  branch_c_expected_DALY <- branch_c_DALY*branch_c
+  branch_d_expected_DALY <- branch_d_DALY*branch_d
+  branch_e_expected_DALY <- branch_e_DALY*branch_e
+  branch_f_expected_DALY <- branch_f_DALY*branch_f
+  branch_g_expected_DALY <- branch_g_DALY*branch_g
+  branch_h_expected_DALY <- branch_h_DALY*branch_h
+  branch_i_expected_DALY <- branch_i_DALY*branch_i
+  
+  # NIRUDAK
+  branch_j_expected_DALY <- branch_j_DALY*branch_j
+  branch_k_expected_DALY <- branch_k_DALY*branch_k
+  branch_l_expected_DALY <- branch_l_DALY*branch_l
+  branch_m_expected_DALY <- branch_m_DALY*branch_m
+  branch_n_expected_DALY <- branch_n_DALY*branch_n
+  branch_o_expected_DALY <- branch_o_DALY*branch_o
+  branch_p_expected_DALY <- branch_p_DALY*branch_p
+  branch_q_expected_DALY <- branch_q_DALY*branch_q
+  branch_r_expected_DALY <- branch_r_DALY*branch_r
+  
+  # calculating ICER
+  # total expected costs
+  WHO_total_expected_cost <- sum(branch_a_expected_cost, branch_b_expected_cost, branch_c_expected_cost, branch_d_expected_cost,
+                                 branch_e_expected_cost, branch_f_expected_cost, branch_g_expected_cost, branch_h_expected_cost,
+                                 branch_i_expected_cost)
+  
+  NIRUDAK_total_expected_cost <- sum(branch_j_expected_cost, branch_k_expected_cost, branch_l_expected_cost, branch_m_expected_cost,
+                                     branch_n_expected_cost, branch_o_expected_cost, branch_p_expected_cost, branch_q_expected_cost,
+                                     branch_r_expected_cost)
+  
+  # total expected DALYs
+  WHO_total_expected_DALYs <- sum(branch_a_expected_DALY, branch_b_expected_DALY, branch_c_expected_DALY, branch_d_expected_DALY,
+                                  branch_e_expected_DALY, branch_f_expected_DALY, branch_g_expected_DALY, branch_h_expected_DALY,
+                                  branch_i_expected_DALY)
+  
+  NIRUDAK_total_expected_DALYs <-sum(branch_j_expected_DALY, branch_k_expected_DALY, branch_l_expected_DALY, branch_m_expected_DALY,
+                                     branch_n_expected_DALY, branch_o_expected_DALY, branch_p_expected_DALY, branch_q_expected_DALY,
+                                     branch_r_expected_DALY)
+  # incremental costs and DALYs
+  incremental_costs <- NIRUDAK_total_expected_cost-WHO_total_expected_cost
+  incremental_DALYs <-NIRUDAK_total_expected_DALYs-WHO_total_expected_DALYs
+  # negative sign preserves usual interpretation of ICER
+  incremental_DALYs_averted <- -incremental_DALYs
+  
+  # ICER
+  ICER = (incremental_costs)/(incremental_DALYs_averted)
+  
+  results_vec <- c(ICER, incremental_costs, incremental_DALYs_averted, incremental_DALYs, NIRUDAK_total_expected_cost, WHO_total_expected_cost, NIRUDAK_total_expected_DALYs, WHO_total_expected_DALYs)
+  
+  names(results_vec) <- c("ICER", "Incremental Costs", "Incremental DALYs Averted", "Incremental DALYs", "NIRUDAK Total Expected Costs", "WHO Total Expected Costs", "NIRUDAK Total Expected DALYs", "WHO Total Expected DALYs")
+  
+  return(results_vec)
+  
+}
 
 ####Unit Costs
 
@@ -386,6 +561,90 @@ data %>% select(str_subset(names(data), "IV")) %>% rowSums()
 data$iv_fluid_prior_to_admit_wt %>% summary()
 
 names(data)
+
+############################ Stata code ########################################
+* SET UP CODE
+* CALCULATING HYPOTHETICAL YEARS OF LIFE LOST FOR ALL PATIENTS (i.e. if they died at their current age)
+gen years_lost_male = 0
+replace years_lost_male = 81.41 if Sex == "Male" & Age < 5
+replace years_lost_male = 76.63 if Sex == "Male" & 5 <= Age & Age < 10
+replace years_lost_male = 71.66 if Sex == "Male" & 10 <= Age & Age < 15
+replace years_lost_male = 66.69 if Sex == "Male" & 15 <= Age & Age < 20
+replace years_lost_male = 61.77 if Sex == "Male" & 20 <= Age & Age < 25
+replace years_lost_male = 56.91 if Sex == "Male" & 25 <= Age & Age < 30
+replace years_lost_male = 52.03 if Sex == "Male" & 30 <= Age & Age < 35
+replace years_lost_male = 47.18 if (Sex == "Male") & 35 <= Age & Age < 40
+replace years_lost_male = 42.35 if Sex == "Male" & 40 <= Age & Age < 45
+replace years_lost_male = 37.57 if Sex == "Male" & 45 <= Age & Age < 50
+replace years_lost_male = 32.89 if Sex == "Male" & 50 <= Age & Age < 55
+replace years_lost_male = 28.34 if Sex == "Male" & 55 <= Age & Age < 60
+replace years_lost_male = 23.97 if Sex == "Male" & 60 <= Age & Age < 65
+replace years_lost_male = 19.83 if Sex == "Male" & 65 <= Age & Age < 70
+replace years_lost_male = 15.96 if Sex == "Male" & 70 <= Age & Age < 75
+replace years_lost_male = 12.41 if Sex == "Male" & 75 <= Age & Age < 80
+replace years_lost_male = 9.18 if Sex == "Male" & 80 <= Age & Age < 85
+replace years_lost_male = 6.46 if Sex == "Male" & 85 <= Age & Age < 90
+replace years_lost_male = 4.41 if Sex == "Male" & 90 <= Age
+
+
+gen years_lost_female = 87.45 if Sex == "Female" & Age < 5
+replace years_lost_female = 82.66 if Sex == "Female" & 5 <= Age & Age < 10
+replace years_lost_female = 77.69 if Sex == "Female" & 10 <= Age & Age < 15
+replace years_lost_female = 72.72 if Sex == "Female" & 15 <= Age & Age < 20
+replace years_lost_female = 67.77 if Sex == "Female" & 20 <= Age & Age < 25
+replace years_lost_female = 62.84 if Sex == "Female" & 25 <= Age & Age < 30
+replace years_lost_female = 57.91 if Sex == "Female" & 30 <= Age & Age < 35
+replace years_lost_female = 53 if Sex == "Female" & 35 <= Age & Age < 40
+replace years_lost_female = 48.11 if Sex == "Female" & 40 <= Age & Age < 45
+replace years_lost_female = 43.26 if Sex == "Female" & 45 <= Age & Age < 50
+replace years_lost_female = 38.49 if Sex == "Female" & 50 <= Age & Age < 55
+replace years_lost_female = 33.79 if Sex == "Female" & 55 <= Age & Age < 60
+replace years_lost_female = 29.17 if Sex == "Female" & 60 <= Age & Age < 65
+replace years_lost_female = 24.63 if Sex == "Female" & 65 <= Age & Age < 70
+replace years_lost_female = 20.21 if Sex == "Female" & 70 <= Age & Age < 75
+replace years_lost_female = 15.97 if Sex == "Female" & 75 <= Age & Age < 80
+replace years_lost_female = 12.01 if Sex == "Female" & 80 <= Age & Age < 85
+replace years_lost_female = 8.51 if Sex == "Female" & 85 <= Age & Age < 90
+replace years_lost_female = 5.71 if Sex == "Female" & 90 <= Age
+
+
+gen all_sex_years_lost_japan = years_lost_male if Sex == "Male"
+replace all_sex_years_lost_japan = years_lost_female if Sex == "Female"
+
+
+********************************************************************************
+  * SETTING UP COST VARIABLES
+* converting string date variables to formatted data types
+gen admit_date = date(AdmitDate, "MDY")
+format admit_date %td
+gen discharge_date = date(Form8DischargeDate, "MDY")
+format admit_date %td
+
+* calculating length of stay
+gen length_stay = discharge_date - admit_date
+
+* daily wage
+gen daily_wage = (MonthlyIncome/30)
+
+* wage lost to hospital stay
+gen wage_lost_hospital_stay = daily_wage * length_stay
+
+* converting lost wages from BDT to USD using purchasing power parity (PPP) from the World Bank
+gen wage_lost_hospital_stay_usd = wage_lost_hospital_stay * 0.031
+
+* total cost variable: NIRUDAK_irr_cost_bdt2 & WHO_irr_cost_bdt2
+* BDT to USD conversion using PPP (in 2019, 31.29 BT per USD)
+* those NIRUDAK_irr_cost_bdt2 & WHO_irr_cost_bdt2 variables are from the file "nirudak_cost_analysis_stata_code.do" — i.e., the initial cost analysis
+gen NIRUDAK_total_cost_USD = NIRUDAK_irr_cost_bdt2 * 0.031
+gen WHO_total_cost_USD = WHO_irr_cost_bdt2 * 0.031
+
+* initial recommended resuscitation (IRR) (in USD) — did not do this because there are no wages lost in IRR
+
+* calculating total costs — full length of hospital stay (in USD)
+gen WHO_cea_cost_total = WHO_total_cost_USD + wage_lost_hospital_stay_usd
+gen NIRUDAK_cea_cost_total = NIRUDAK_total_cost_USD + wage_lost_hospital_stay_usd
+
+############################ end Stata code ####################################
 
 
 
