617db1a0567096e1e7b6ae8a244b1ee0e311bf63
Anagha Lokhande <alokhande21@gmail.com>
2024-10-18 13:58:13 -0400

    setting up YLL

--- code/PSA.R
+++ code/PSA.R
@@ -325,77 +325,25 @@ resid_life_expect_at_age_female <- c(87.45, 82.66, 77.69, 72.72, 67.77, 62.84, 5
                                     53, 48.11, 43.26, 38.49, 33.79, 29.17, 24.63, 20.21, 15.97,
                                     12.01, 8.51, 5.71)
 
-
 resid_life_interp_fxn_male <- splinefun(five_yr_age_buckets, resid_life_expect_at_age_male)
 
 resid_life_interp_fxn_female <- splinefun(five_yr_age_buckets, resid_life_expect_at_age_female)
 
 
-  gen years_lost_female = 87.45 if Sex == "Female" & Age < 5
-replace years_lost_female = 82.66 if Sex == "Female" & 5 <= Age & Age < 10
-replace years_lost_female = 77.69 if Sex == "Female" & 10 <= Age & Age < 15
-replace years_lost_female = 72.72 if Sex == "Female" & 15 <= Age & Age < 20
-replace years_lost_female = 67.77 if Sex == "Female" & 20 <= Age & Age < 25
-replace years_lost_female = 62.84 if Sex == "Female" & 25 <= Age & Age < 30
-replace years_lost_female = 57.91 if Sex == "Female" & 30 <= Age & Age < 35
-replace years_lost_female = 53 if Sex == "Female" & 35 <= Age & Age < 40
-replace years_lost_female = 48.11 if Sex == "Female" & 40 <= Age & Age < 45
-replace years_lost_female = 43.26 if Sex == "Female" & 45 <= Age & Age < 50
-replace years_lost_female = 38.49 if Sex == "Female" & 50 <= Age & Age < 55
-replace years_lost_female = 33.79 if Sex == "Female" & 55 <= Age & Age < 60
-replace years_lost_female = 29.17 if Sex == "Female" & 60 <= Age & Age < 65
-replace years_lost_female = 24.63 if Sex == "Female" & 65 <= Age & Age < 70
-replace years_lost_female = 20.21 if Sex == "Female" & 70 <= Age & Age < 75
-replace years_lost_female = 15.97 if Sex == "Female" & 75 <= Age & Age < 80
-replace years_lost_female = 12.01 if Sex == "Female" & 80 <= Age & Age < 85
-replace years_lost_female = 8.51 if Sex == "Female" & 85 <= Age & Age < 90
-replace years_lost_female = 5.71 if Sex == "Female" & 90 <= Age
-
-  
-  
-gen years_lost_male = 0
-replace years_lost_male = 81.41 if Sex == "Male" & Age < 5
-replace years_lost_male = 76.63 if Sex == "Male" & 5 <= Age & Age < 10
-replace years_lost_male = 71.66 if Sex == "Male" & 10 <= Age & Age < 15
-replace years_lost_male = 66.69 if Sex == "Male" & 15 <= Age & Age < 20
-replace years_lost_male = 61.77 if Sex == "Male" & 20 <= Age & Age < 25
-replace years_lost_male = 56.91 if Sex == "Male" & 25 <= Age & Age < 30
-replace years_lost_male = 52.03 if Sex == "Male" & 30 <= Age & Age < 35
-replace years_lost_male = 47.18 if (Sex == "Male") & 35 <= Age & Age < 40
-replace years_lost_male = 42.35 if Sex == "Male" & 40 <= Age & Age < 45
-replace years_lost_male = 37.57 if Sex == "Male" & 45 <= Age & Age < 50
-replace years_lost_male = 32.89 if Sex == "Male" & 50 <= Age & Age < 55
-replace years_lost_male = 28.34 if Sex == "Male" & 55 <= Age & Age < 60
-replace years_lost_male = 23.97 if Sex == "Male" & 60 <= Age & Age < 65
-replace years_lost_male = 19.83 if Sex == "Male" & 65 <= Age & Age < 70
-replace years_lost_male = 15.96 if Sex == "Male" & 70 <= Age & Age < 75
-replace years_lost_male = 12.41 if Sex == "Male" & 75 <= Age & Age < 80
-replace years_lost_male = 9.18 if Sex == "Male" & 80 <= Age & Age < 85
-replace years_lost_male = 6.46 if Sex == "Male" & 85 <= Age & Age < 90
-replace years_lost_male = 4.41 if Sex == "Male" & 90 <= Age
-
-
-gen years_lost_female = 87.45 if Sex == "Female" & Age < 5
-replace years_lost_female = 82.66 if Sex == "Female" & 5 <= Age & Age < 10
-replace years_lost_female = 77.69 if Sex == "Female" & 10 <= Age & Age < 15
-replace years_lost_female = 72.72 if Sex == "Female" & 15 <= Age & Age < 20
-replace years_lost_female = 67.77 if Sex == "Female" & 20 <= Age & Age < 25
-replace years_lost_female = 62.84 if Sex == "Female" & 25 <= Age & Age < 30
-replace years_lost_female = 57.91 if Sex == "Female" & 30 <= Age & Age < 35
-replace years_lost_female = 53 if Sex == "Female" & 35 <= Age & Age < 40
-replace years_lost_female = 48.11 if Sex == "Female" & 40 <= Age & Age < 45
-replace years_lost_female = 43.26 if Sex == "Female" & 45 <= Age & Age < 50
-replace years_lost_female = 38.49 if Sex == "Female" & 50 <= Age & Age < 55
-replace years_lost_female = 33.79 if Sex == "Female" & 55 <= Age & Age < 60
-replace years_lost_female = 29.17 if Sex == "Female" & 60 <= Age & Age < 65
-replace years_lost_female = 24.63 if Sex == "Female" & 65 <= Age & Age < 70
-replace years_lost_female = 20.21 if Sex == "Female" & 70 <= Age & Age < 75
-replace years_lost_female = 15.97 if Sex == "Female" & 75 <= Age & Age < 80
-replace years_lost_female = 12.01 if Sex == "Female" & 80 <= Age & Age < 85
-replace years_lost_female = 8.51 if Sex == "Female" & 85 <= Age & Age < 90
-replace years_lost_female = 5.71 if Sex == "Female" & 90 <= Age
+age, sex, branch outcome
+data_use[data_use$male == 1,"age"] 
+data_use$branch <- 1
+actual_dehydrat_cat
+nirudak_dehydrat_cat
 
 # will at some point need to go back and change ICER to be consistent new assumptions made here
+
+# note in manuscript: YLL is the mean among all patients of a branch in which patients die
+# e.g., true Severe dehydration but model-assigned "No" or "Some" dehydration
+# check Jonah's code w/ statistical tests comparing the distribution of ages
+# across branches between the two models
+# he did find statistically sig. differences in age between branches (in exploratory_analysis_of_trial_data.R)
+# can run the analysis both ways (with mean YLL across whole sample and with average age in each branch)
 
 ##Actual
 IV_fluid_prior_to_admit_wt
